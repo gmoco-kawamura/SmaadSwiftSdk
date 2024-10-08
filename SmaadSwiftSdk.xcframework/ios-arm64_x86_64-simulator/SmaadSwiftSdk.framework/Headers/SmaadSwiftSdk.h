173d3f6eb44232da5c56363ad170c40b07732a8d
gmoco-kawamura <kawamura-takuya@gmo-connect.jp>
2024-10-08 11:36:52 +0900

    Add Swift Package & XCFramework

---
+++ SmaadSwiftSdk.xcframework/ios-arm64_x86_64-simulator/SmaadSwiftSdk.framework/Headers/SmaadSwiftSdk.h
@@ -0,0 +1,18 @@
+//
+//  SmaadSwiftSdk.h
+//  SmaadSwiftSdk
+//
+//  Created by 川村拓也 on 2024/10/08.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SmaadSwiftSdk.
+FOUNDATION_EXPORT double SmaadSwiftSdkVersionNumber;
+
+//! Project version string for SmaadSwiftSdk.
+FOUNDATION_EXPORT const unsigned char SmaadSwiftSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmaadSwiftSdk/PublicHeader.h>
+
+
